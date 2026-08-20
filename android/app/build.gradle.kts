cc89a6fc0a5a3c902748dbc68e66498bd4a94257
soskksjss <mr.hacker5354@gmail.com>
2026-08-20 10:20:21 +0000

    Fixed Gradle 8.14 and added bypass flag for Codemagic

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,35 +3,30 @@ plugins {
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
-
 android {
     namespace = "com.example.project_qwen"
-    compileSdk = 34
-
+    compileSdk = flutter.compileSdkVersion.toInt()
+    ndkVersion = flutter.ndkVersion
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
     defaultConfig {
         applicationId = "com.example.project_qwen"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        minSdk = flutter.minSdkVersion.toInt()
+        targetSdk = flutter.targetSdkVersion.toInt()
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName.toString()
     }
-
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
-
 flutter {
     source = "../.."
 }
